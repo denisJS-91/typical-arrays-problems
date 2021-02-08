c1f4cb9f42f920c746f71f20da273bde053792c2
denisJS-91 <77659339+denisJS-91@users.noreply.github.com>
2021-02-08 13:00:08 +0300

    index.js:Changed the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array == 0) return 0;
+    let arr_min = array[0];
+    for (i = 1; i <= array.length; i++) {
+        if (array[i] <= arr_min) {
+            arr_min = array[i];
+        }
+    }
+    return arr_min;  
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array == 0) return 0;
+    let arr_max = array[0];
+    for (i = 1; i < array.length; i++) {
+        if (arr_max <= array[i]) {
+            arr_max = array[i];
+        }
+    }
+    return arr_max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
