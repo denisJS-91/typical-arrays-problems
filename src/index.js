9125d305c15112168f4b3598cbb78d62caf6bc6e
denisJS-91 <77659339+denisJS-91@users.noreply.github.com>
2021-02-08 13:18:28 +0300

    index.js:Changed camelCase Language

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 
 exports.min = function min (array) {
     if (array == undefined || array == 0) return 0;
-    let arr_min = array[0];
+    let arrMin = array[0];
     for (i = 1; i <= array.length; i++) {
-        if (array[i] <= arr_min) {
-            arr_min = array[i];
+        if (array[i] <= arrMin) {
+            arrMin = array[i];
         }
     }
-    return arr_min;  
+    return arrMin;  
 }
 
 exports.max = function max (array) {
     if (array == undefined || array == 0) return 0;
-    let arr_max = array[0];
+    let arrMax = array[0];
     for (i = 1; i < array.length; i++) {
-        if (arr_max <= array[i]) {
-            arr_max = array[i];
+        if (arrMax <= array[i]) {
+            arrMax = array[i];
         }
     }
-    return arr_max;
+    return arrMax;
 }
 
 exports.avg = function avg (array) {
